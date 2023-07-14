4e4dcff8d8ac805c639f20d1a3ff3ba59724a050
roman lepihov <lepihovrom@yandex.by>
2023-07-14 10:57:54 +0300

    [update] дополнения вьюшки

--- src/main/java/View/View.java
+++ src/main/java/View/View.java
@@ -1,7 +1,5 @@
 package View;
 
-//import Game.Units.Hero;
-
 import arena.ArenaMessage;
 import units.*;
 import units.abstractUnits.Unit;
@@ -13,12 +11,6 @@ import arena.map.Map;
 
 public class View {
     private Map map;
-
-    //    private static int step = 1;
-//    private static final int[] l = {0};
-//    private static final String top10 = formatDiv("a") + String.join("", Collections.nCopies(9, formatDiv("-b"))) + formatDiv("-c");
-//    private static final String midl10 = formatDiv("d") + String.join("", Collections.nCopies(9, formatDiv("-e"))) + formatDiv("-f");
-//    private static final String bottom10 = formatDiv("g") + String.join("", Collections.nCopies(9, formatDiv("-h"))) + formatDiv("-i");
 
     public View(Map map) {
         this.map = map;
@@ -45,21 +37,10 @@ public class View {
 
     private String getChar(int x, int y) {
         String out = "|   ";
-//        for (Unit unit : team) {
-//                if (human.getCoords()[0] == x && human.getCoords()[1] == y){
-//                    if (human.getHp() == 0) {
-//                        out = "|" + (AnsiColors.ANSI_RED + human.toString().charAt(0) + AnsiColors.ANSI_RESET);
-//                        break;
-//                    }
-//                    if (Main.team1.contains(human)) out = "|" + (AnsiColors.ANSI_GREEN + human.toString().charAt(0) + AnsiColors.ANSI_RESET);
-//                    if (Main.team2.contains(human)) out = "|" + (AnsiColors.ANSI_BLUE + human.toString().charAt(0) + AnsiColors.ANSI_RESET);
-//                    break;
-//                }
-//        }
 
         Object field = this.map.getField(x, y);
 
-        String sym = "";
+        String sym = "   ";
 
         if (field instanceof Crossbowman)
             sym = "Crb";
@@ -81,8 +62,6 @@ public class View {
             sym = "Spn";
         else if (field instanceof Wizard)
             sym = "Wzd";
-        else
-            sym = "   ";
 
         if (field instanceof Unit) {
             out = "|" + (getAnsiColor(((Unit) field).getTeam().color) + sym + AnsiColors.ANSI_RESET);
@@ -99,38 +78,24 @@ public class View {
         }
 
         System.out.print(AnsiColors.ANSI_YELLOW + "Step " + step + AnsiColors.ANSI_RESET);
-        //teams.forEach((v) -> l[0] = Math.max(l[0], v.getSize()));
-        //System.out.print("_".repeat(l[0] * 2));
         System.out.println();
 
         //вывод верха таблицы
         System.out.print(formatDiv("a") + formatDiv("-") + String.join("", Collections.nCopies(map.sizeX - 1, formatDiv("-") + formatDiv("-b") + formatDiv("-"))) + formatDiv("-") + formatDiv("-c") + "    ");
 
         for (Team team : teams) {
-            //System.out.print(AnsiColors.ANSI_GREEN + ":\tGreen side" + AnsiColors.ANSI_RESET);
-            //for (int i = 0; i < l[0] - 9; i++)
-            //    System.out.println(" ".repeat(l[0] - 9));
             System.out.print(getAnsiColor(team.color) + team.name + AnsiColors.ANSI_RESET);
             tabSetter(team.name.length(), 70);
         }
         System.out.println();
-        //System.out.print(AnsiColors.ANSI_GREEN + ":\tGreen side" + AnsiColors.ANSI_RESET);
-        //for (int i = 0; i < l[0] - 9; i++)
-        //    System.out.println(" ".repeat(l[0] - 9));
-//        System.out.println(AnsiColors.ANSI_BLUE + "Blue side" + AnsiColors.ANSI_RESET);
-
 
         for (int i = 0; i < map.sizeX; i++) {
             System.out.print(getChar(i, 0));
         }
         System.out.print("|    ");
-        //System.out.print(teams.get(0).name);
-        //tabSetter(teams.get(0).getSize(), l[0]);
-        //System.out.println(teams.get(1).name);
-
         System.out.println();
 
-        // вывод серидины таблицы
+        // вывод середины таблицы
         System.out.println(formatDiv("d") + formatDiv("-") + String.join("", Collections.nCopies(map.sizeX - 1, formatDiv("-") + formatDiv("-e") + formatDiv("-"))) + formatDiv("-") + formatDiv("-f"));
 
         for (int i = 1; i < map.sizeY - 1; i++) {
@@ -158,16 +123,15 @@ public class View {
             System.out.print(getChar(j, map.sizeY - 1));
         }
         System.out.println("|    ");
-//            System.out.print(Main.team1.get(9).getInfo());
-//            tabSetter(Main.team1.get(9).getInfo().length(), l[0]);
-//            System.out.println(Main.team2.get(9).getInfo());
         //вывод низа таблицы
         System.out.println(formatDiv("g") + formatDiv("-") + String.join("", Collections.nCopies(map.sizeX - 1, formatDiv("-") + formatDiv("-h") + formatDiv("-"))) + formatDiv("-") + formatDiv("-i"));
 
-        for (ArenaMessage msg: arenaMessages) {
-            if (msg.unit != null) System.out.print(getAnsiColor(msg.unit.getTeam().color) +  msg.unit.getType() + " " + msg.unit.getName() + AnsiColors.ANSI_RESET);
+        for (ArenaMessage msg : arenaMessages) {
+            if (msg.unit != null)
+                System.out.print(getAnsiColor(msg.unit.getTeam().color) + msg.unit.getType() + " " + msg.unit.getName() + AnsiColors.ANSI_RESET);
             if (msg.message != null) System.out.print(msg.message);
-            if (msg.target != null) System.out.print(getAnsiColor(msg.target.getTeam().color) + msg.target.getType() + " " + msg.target.getName() + AnsiColors.ANSI_RESET);
+            if (msg.target != null)
+                System.out.print(getAnsiColor(msg.target.getTeam().color) + msg.target.getType() + " " + msg.target.getName() + AnsiColors.ANSI_RESET);
             System.out.println();
         }
         arenaMessages.clear();
